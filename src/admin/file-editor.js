4895206330dfefceef7e831e969b52652d92c95b
Nathan Nau <APYBJ@agfahealthcare.com>
2019-08-01 16:22:26 +0200

    tttt

--- src/admin/file-editor.js
+++ src/admin/file-editor.js
@@ -2,6 +2,8 @@
 
 var Vue = require('vue');
 require('./drop-zone');
+var $ = require("jquery");
+var hljs = require('highlight.js')
 
 module.exports = Vue.component('file-editor', { 
     template: require('./file-editor.html'),
@@ -10,6 +12,7 @@ module.exports = Vue.component('file-editor', {
         filename: String,
         content: String,
         type: String,
+        extension: String,
     },
     data: function() { 
     //     var item = null;
@@ -24,13 +27,63 @@ module.exports = Vue.component('file-editor', {
     //         controles: this.controles,
         };
     },
+    mounted: function() {
+        console.log("mounted")
+        console.log(this.$refs.code.innerHTML)
+        hljs.highlightBlock(this.$refs.code);
+    },
     methods: {
-        switchEditor: function() { this.editing = !this.editing; },
-        drop: function(ev) { 
-            console.log('TODO fe.drop !', arguments); 
+        cntInput: function() { console.log('cntInput'); },
+        // cntChange: function() { console.log('cntChange'); },
+
+        switchEditor: function() { 
+            // console.log('Switch !'); 
+            this.editing = !this.editing; 
         },
-        download: function() { console.log('TODO download !', arguments); },
-        upload: function() { console.log('TODO upload !', arguments); },
+        drop: function(content) { 
+            //console.log('TODO fe.drop !', arguments); 
+            this.$emit('input', content);
+        },
+        download: function() { 
+            // console.log('TODO download !', arguments); 
+            var content = new Blob([this.content], {type: 'application/octet-stream' /*'text/plain'*/});
+            console.log(content);
+            var url = window.URL.createObjectURL(content);
+
+            var lnk = $('<a />').attr({
+                href:url,
+                download: this.filename,
+            }).css({ display:'none'});
+            lnk.appendTo('body');
+            lnk[0].click();
+            lnk.remove();
+            window.URL.revokeObjectURL(url);
+        },
+        upload: function() { 
+            // console.log('TODO upload !', arguments); 
+            var inputFile = $('<input />').attr({
+                type: 'file',
+                accept: this.extension,
+            }).css({ display:'none'});
+            inputFile.appendTo('body');
+            var that = this;
+            inputFile[0].onchange = async function(ev) {
+                var files = ev.target.files;
+                if (!files.length) return;
+
+                var file = files[0];
+                var reader = new FileReader();
+                var readerPromise = new Promise((resolve, reject)=>{
+                    reader.onload = function (evt) { resolve(evt.target.result); }
+                    reader.onerror = function (evt) { reject(evt); }
+                });
+                reader.readAsText(file, "UTF-8");
+                var content = await readerPromise;
+                that.$emit('input', content);
+            }
+            inputFile[0].click();
+            inputFile.remove();
+        },
     }
     // watch: {
     //     item: { deep:true, handler: function(value) { 
